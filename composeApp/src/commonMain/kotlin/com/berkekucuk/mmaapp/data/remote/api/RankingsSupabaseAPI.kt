93f018c8bd7822e2c37cd46208431d82d9b00b1a
berkekucuk <berke653.bk@gmail.com>
2026-03-02 16:38:15 +0300

    feat: Refactor Rankings API

--- composeApp/src/commonMain/kotlin/com/berkekucuk/mmaapp/data/remote/api/RankingsSupabaseAPI.kt
+++ composeApp/src/commonMain/kotlin/com/berkekucuk/mmaapp/data/remote/api/RankingsSupabaseAPI.kt
@@ -4,23 +4,21 @@ import com.berkekucuk.mmaapp.data.remote.dto.RankingDto
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.postgrest.from
 import io.github.jan.supabase.postgrest.query.Columns
-import io.github.jan.supabase.postgrest.query.Order
 
 class RankingsSupabaseAPI(
     private val client: SupabaseClient
 ) : RankingsRemoteDataSource {
 
+    private val columnsToSelect = """
+            weight_class_id,
+            rank_number,
+            fighters(fighter_id, name, image_url, record, country_code),
+            weight_classes(name, sort_order)
+    """.trimIndent().replace("\n", "").replace(" ", "")
+
     override suspend fun fetchRankings(): List<RankingDto> {
         return client.from("rankings").select(
-            columns = Columns.raw("""
-                weight_class_id,
-                rank_number,
-                fighter_id,
-                fighters(fighter_id, name, nickname, image_url, record, country_code),
-                weight_classes(id, name, sort_order, weight_limit)
-            """.trimIndent().replace("\n", ""))
-        ) {
-            order(column = "rank_number", order = Order.ASCENDING)
-        }.decodeList<RankingDto>()
+            columns = Columns.raw(columnsToSelect)
+        ).decodeList<RankingDto>()
     }
 }
