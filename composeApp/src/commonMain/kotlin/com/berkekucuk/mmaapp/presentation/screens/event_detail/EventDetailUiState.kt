78e8ba99f0aa3084971cea2e5393fa28d6fac090
berkekucuk <berke653.bk@gmail.com>
2026-03-23 18:35:36 +0300

    refactor: streamline EventDetailViewModel and update error handling in EventDetailUiState

--- composeApp/src/commonMain/kotlin/com/berkekucuk/mmaapp/presentation/screens/event_detail/EventDetailUiState.kt
+++ composeApp/src/commonMain/kotlin/com/berkekucuk/mmaapp/presentation/screens/event_detail/EventDetailUiState.kt
@@ -1,13 +1,15 @@
 package com.berkekucuk.mmaapp.presentation.screens.event_detail
 
 import com.berkekucuk.mmaapp.domain.model.Event
-import com.berkekucuk.mmaapp.domain.model.Fight
 
 data class EventDetailUiState(
     val isLoading: Boolean = true,
     val isRefreshing: Boolean = false,
-    val allFights: List<Fight> = emptyList(),
     val event: Event? = null,
-    val mainCardFights: List<Fight> = emptyList(),
-    val prelimFights: List<Fight> = emptyList()
+    val error: EventDetailError? = null,
 )
+
+enum class EventDetailError {
+    NETWORK_ERROR,
+    UNKNOWN_ERROR,
+}
