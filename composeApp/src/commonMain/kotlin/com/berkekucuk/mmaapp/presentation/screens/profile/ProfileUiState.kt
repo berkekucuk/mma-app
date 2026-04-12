1a8ed5328236c63d27d360da4cbe29e15444f21a
berkekucuk <berke653.bk@gmail.com>
2026-04-12 21:34:37 +0300

    refactor: update user data model and navigation in profile screens

--- composeApp/src/commonMain/kotlin/com/berkekucuk/mmaapp/presentation/screens/profile/ProfileUiState.kt
+++ composeApp/src/commonMain/kotlin/com/berkekucuk/mmaapp/presentation/screens/profile/ProfileUiState.kt
@@ -1,11 +1,9 @@
 package com.berkekucuk.mmaapp.presentation.screens.profile
 
-import com.berkekucuk.mmaapp.domain.model.AuthState
-import com.berkekucuk.mmaapp.domain.model.Profile
+import com.berkekucuk.mmaapp.domain.model.User
 
 data class ProfileUiState(
-    val authState: AuthState = AuthState.Loading,
     val isLoading: Boolean = true,
     val isRefreshing: Boolean = false,
-    val profile: Profile? = null,
+    val user: User? = null,
 )
