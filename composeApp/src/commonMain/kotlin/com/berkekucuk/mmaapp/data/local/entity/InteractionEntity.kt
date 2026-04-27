9307d4f8ba139eb7c2e8f27cb15df036e4046e0b
berkekucuk <berke653.bk@gmail.com>
2026-04-27 23:40:57 +0300

    refactor: add CASCADE delete foreign keys to predictions, fight notifications, and interactions tables relating to user entities

--- composeApp/src/commonMain/kotlin/com/berkekucuk/mmaapp/data/local/entity/InteractionEntity.kt
+++ composeApp/src/commonMain/kotlin/com/berkekucuk/mmaapp/data/local/entity/InteractionEntity.kt
@@ -2,9 +2,20 @@ package com.berkekucuk.mmaapp.data.local.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "user_fighter_interactions")
+@Entity(
+    tableName = "user_fighter_interactions",
+    foreignKeys = [
+        ForeignKey(
+            entity = UserEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["user_id"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class InteractionEntity(
     @PrimaryKey val id: String,
     @ColumnInfo(name = "user_id") val userId: String,
